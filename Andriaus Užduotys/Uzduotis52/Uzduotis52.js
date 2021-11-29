3d58ec576692b73faa27d6fea69d28561ffc4ffd
unknown <indre.irbinskaite@gmail.com>
2021-11-29 11:44:01 +0200

    second step

--- Andriaus Užduotys/Uzduotis52/Uzduotis52.js
+++ Andriaus Užduotys/Uzduotis52/Uzduotis52.js
@@ -1,2 +1,8 @@
 const generate = document.querySelector(".generator")
+const main = document.querySelector(".main")
+
+generate.onclick = () => {
+    let randNum = Math.random()*10
+    main.innerHTML = `${randNum}`
+}
 
